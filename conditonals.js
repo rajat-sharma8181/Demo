773cdb08e5cff7eb043f8ec64061671a4cbfcbb3
rajat-sharma8181 <rajats12120@gmail.com>
2024-03-27 18:30:17 +0530

    Code Added

---
+++ conditonals.js
@@ -0,0 +1,19 @@
+/**var num = 101
+if(num%2==0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
+console.log("congratulations"); **/
+
+var x=10;
+if(x>=0){
+    if(x>5){
+        console.log("x is greater than 0 & 5")
+        console.log("x = ",x)
+    }else{
+        console.log("x is greater than 0 but less than 5")
+    }
+}else{
+    console.log("x is less than 0")
+}
---
+++ datatype.js
@@ -0,0 +1,7 @@
+/**var i = 1
+console.log(i++);
+console.log(i)**/
+
+var i = 1
+console.log(++i);
+console.log(i);
